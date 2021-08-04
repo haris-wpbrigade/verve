55405e7f1569e57c186cc1781dc586294cdd9ae6
haris-wpbrigade <87003725+haris-wpbrigade@users.noreply.github.com>
2021-08-03 23:52:30 -0700

    Desktop Design

--- js/main.js
+++ js/main.js
@@ -4,3 +4,15 @@ const NavMenu = document.querySelector('.nav-menu')
 ToggleBtn.addEventListener('click', () => {
     NavMenu.classList.toggle('active')
 })
+
+const BackGroundIMG = document.querySelector('.content-body')
+
+const setBackIMG = (imgsrc) => {
+    // console.log(imgsrc)
+    BackGroundIMG.style.backgroundImage = `url(../img/${imgsrc})`;
+}
+
+const resetBackIMG = (imgsrc) => {
+    // console.log(imgsrc)
+    BackGroundIMG.style.backgroundImage = `url(../img/${imgsrc})`;
+}
