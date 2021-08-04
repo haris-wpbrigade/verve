78521efb19376b07cb625154e3c2dcb28ab53d10
haris-wpbrigade <87003725+haris-wpbrigade@users.noreply.github.com>
2021-08-04 05:11:27 -0700

    Update Desktop and Mobile Design

--- js/main.js
+++ js/main.js
@@ -1,15 +1,34 @@
 const ToggleBtn = document.querySelector('.toggleBtn')
 const NavMenu = document.querySelector('.nav-menu')
+const Overlay =  document.querySelector('.overlay')
 
 ToggleBtn.addEventListener('click', () => {
     NavMenu.classList.toggle('active')
     ToggleBtn.classList.toggle('active')
+    Overlay.classList.toggle('active')
 })
 
-const BackGroundIMG = document.querySelector('.content-body')
+Overlay.addEventListener('click', () => {
+    NavMenu.classList.remove('active')
+    ToggleBtn.classList.remove('active')
+    Overlay.classList.remove('active')
+})
 
+// on hover Content Changed
+
+const BackVideo = document.getElementById("myVideo")
+const PlayBackVideo = (Videosrc) => {
+    BackVideo.src = `../img/${Videosrc}`
+    BackVideo.style.visibility = "visible"
+    BackVideo.style.opacity = "1"
+    BackVideo.play()
+}
+
+const BackGroundIMG = document.querySelector('.content-body')
 const setBackIMG = (imgsrc) => {
-    // console.log(imgsrc)
+    BackVideo.style.visibility = "hidden"
+    BackVideo.style.opacity = "0"
+    BackVideo.pause()
     BackGroundIMG.style.backgroundImage = `url(../img/${imgsrc})`;
 }
 
